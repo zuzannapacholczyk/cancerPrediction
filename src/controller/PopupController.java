235d5999e578a0a447ff7a3d629c563a96c563cd
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2015-12-30 12:39:01 +0100

    popup controller implemented

--- src/controller/PopupController.java
+++ src/controller/PopupController.java
@@ -1,36 +1,58 @@
 package controller;
 
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import weka.ProbabilityCounter;
 
 public class PopupController {
 
 	@FXML
 	private TextField textField;
-	
+
 	@FXML
 	private Button findButton;
-	
+
 	@FXML
 	private Button cancelButton;
-	
+
 	@FXML
 	private Button okButton;
 	
+	File selectedFile = null;
+	
+	@FXML
+	private void initialize() {
+		textField.setEditable(false);
+	}
+
 	@FXML
 	private void cancelButtonClicked(ActionEvent event) {
-		// TODO
+		ProbabilityCounter counter = new ProbabilityCounter();
+		cancelButton.getScene().getWindow().hide();
 	}
-	
+
 	@FXML
 	private void okButtonClicked(ActionEvent event) {
-		// TODO
+		if(selectedFile != null) {
+			ProbabilityCounter counter = new ProbabilityCounter(selectedFile);
+		}
+		else {
+			ProbabilityCounter counter = new ProbabilityCounter();
+		}
+		okButton.getScene().getWindow().hide();
 	}
-	
+
 	@FXML
 	private void findButtonClicked(ActionEvent event) {
-		// TODO
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Wybierz plik z danymi");
+		fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("ARFF", "*.arff*"));
+		File selectedFile = fileChooser.showOpenDialog(findButton.getScene().getWindow());
+		textField.setText(selectedFile.getAbsolutePath());
 	}
 }
--- src/weka/ProbabilityCounter.java
+++ src/weka/ProbabilityCounter.java
@@ -1,5 +1,22 @@
 package weka;
 
+import java.io.File;
+
 public class ProbabilityCounter {
+
+	private File dataFile;
+	
+	public ProbabilityCounter(File selectedFile) {
+		dataFile = selectedFile;
+	}
+
+	public ProbabilityCounter() {
+		readDefaultDataFile();
+	}
+
+	private void readDefaultDataFile() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
