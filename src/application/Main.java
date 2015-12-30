04c9f8fdb1dd3e48712309320870b8c909afb2ef
Zuzanna Pacholczyk <zuzannapacholczyk@wp.pl>
2015-12-30 11:58:34 +0100

    added popup to choose file with data

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +16,19 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Analiza zagrozenia rakiem piersi");
+
+			// popup do wyboru pliku
+			Parent popup = FXMLLoader.load(getClass().getResource("popup.fxml"));
+			Scene popupScene = new Scene(popup);
+			popupScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Stage popupStage = new Stage();
+			popupStage.setScene(popupScene);
+			popupStage.initModality(Modality.APPLICATION_MODAL);
+			popupStage.setTitle("Wybierz plik z danymi");
+			
 			primaryStage.show();
+			popupStage.showAndWait();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/controller/PopupController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class PopupController {
+
+	@FXML
+	private TextField textField;
+	
+	@FXML
+	private Button findButton;
+	
+	@FXML
+	private Button cancelButton;
+	
+	@FXML
+	private Button okButton;
+	
+	@FXML
+	private void cancelButtonClicked(ActionEvent event) {
+		// TODO
+	}
+	
+	@FXML
+	private void okButtonClicked(ActionEvent event) {
+		// TODO
+	}
+	
+	@FXML
+	private void findButtonClicked(ActionEvent event) {
+		// TODO
+	}
+}
